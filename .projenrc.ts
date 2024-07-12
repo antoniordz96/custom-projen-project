a350c91439d07aad70e4949e9647ae0b693c358a
Antonio Rodriguez <antonio.d.rodriguez00@delta.com>
2024-07-11 20:48:33 -0600

    fix: disable releases for now

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,7 +7,8 @@ const project = new cdk.JsiiProject({
   name: 'custom-projen-project',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:antoniordz96/custom-projen-project.git',
-
+  release: false,
+  publishDryRun: true,
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
