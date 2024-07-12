5903f25added3ebeb29a905d30366e31632ed70f
Antonio Rodriguez <antonio.d.rodriguez00@delta.com>
2024-07-11 20:44:14 -0600

    feat: add docker compose

--- .projenrc.ts
+++ .projenrc.ts
@@ -11,6 +11,5 @@ const project = new cdk.JsiiProject({
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
---
+++ docker/README.md
@@ -0,0 +1,13 @@
+# Local Registries via Docker Compose
+
+## Reposilite
+
+- Documentation: <https://reposilite.com/>
+- Local Registry Login: `admin:projen`
+- Endpoint: <http://localhost:8081/>
+
+## Verdacio
+
+- Documentation: <https://verdaccio.org/>
+- Local Registry Login: `admin:projen`
+- Endpoint: <http://localhost:8082/>
